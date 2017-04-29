ebba2377ca58691bc0db99d2f89d2a4337a45786
Alexander Holman MBCS AMIAP <alexander@holman.org.uk>
2017-04-29 16:04:06 +0100

    add hello world

--- come-fly-with-us-api.py
+++ come-fly-with-us-api.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def routes():
+    output = {}
+    for rule in app.url_map.iter_rules():
+        fn = app.view_functions[rule.endpoint]
+        doc = fn.__doc__
+        if rule.rule.startswith('/'):
+            if  doc != None:
+                output[rule.rule] = doc.strip()
+            else:
+                output[rule.rule] = "No docs found"
+    return output
 
+@app.route('/')
+def index():
+    return jsonify({
+        'success': True,
+        'version': 0.1,
+        'methods': routes()
+    })
+
+@app.route('/hello-world')
+def helloWorld():
+    returnDict = {
+        "mssg": "hello world"
+    }
+    return jsonify({
+        'success': True,
+        'version': 0.1,
+        'data': returnDict
+    })
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,host="0.0.0.0",threaded=True)
