9b3ab03e3dc7089128fdfd0cf939461507664e96
Alexander Holman MBCS AMIAP <alexander@holman.org.uk>
2017-04-29 17:20:41 +0100

    add google api support

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import os
 
 from flask import Flask, jsonify, request, Response
 
+from googleplaces import GooglePlaces, types, lang
+
+YOUR_API_KEY = 'AIzaSyAUecZjZRw1lU4N0gIkQR07hG4tcV3AbOY'
+
+google_places = GooglePlaces(YOUR_API_KEY)
+
 app = Flask(__name__)
 
 version = 0.1
@@ -36,6 +42,44 @@ def helloWorld():
     'data': {
       "mssg": "hello world"
     }
+  })
+
+@app.route('/google/<lat>/<lng>')
+def googleTest(lat, lng):
+  """google test"""
+  places = []
+  query_result = google_places.nearby_search(
+    lat_lng={"lat":lat, "lng": lng},
+    radius=20000, types=[types.TYPE_FOOD])
+
+  if query_result.has_attributions:
+      print(query_result.html_attributions)
+
+  for place in query_result.places:
+      place.get_details()
+      place_details = {
+          "name": place.name,
+          "position": {
+              "lat": float(place.geo_location["lat"]),
+              "lng": float(place.geo_location["lng"])
+          },
+          # "details": place.details,
+          # "phone": {
+          #     "local": place.local_phone_number,
+          #     "internation": place.international_phone_number
+          # },
+          # "url": place.url,
+          # "photos": []
+      }
+      # for photo in place.photos:
+      #     photo.get(maxheight=500, maxwidth=500)
+      #     place_details["photos"].append(photo.url)
+      places.append(place_details)
+
+  return jsonify({
+    'success': True,
+    'version': version,
+    'data': places
   })
 
 
